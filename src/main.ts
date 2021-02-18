4037c4190e96bc291c3feb05cb76d254c481b61d
clz <coloz999@gmail.com>
2021-02-18 11:13:34 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import expressSession from 'express-session';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { Prisma } from '@prisma/client';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // app.use(
+  //   expressSession({
+  //     cookie: {
+  //       maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+  //      },
+  //      secret: 'a santa at nasa',
+  //      store: new PrismaSessionStore(
+  //        Prisma,
+  //        {
+  //          checkPeriod: 2 * 60 * 1000,  //ms
+  //          dbRecordIdIsSessionId: true,
+  //          dbRecordIdFunction: undefined,
+  //        }
+  //      )
+  //   }),
+  // );
 
   if (module.hot) {
     module.hot.accept();
